55035340fd9f1214aa16ebef4f79154d64d22fcd
itamarhanan <itamarhanan@hilma.tech>
2026-05-12 08:32:08 +0300

    fix(client): fix mjs not recogninizing meta dirname on non-esm

--- apps/www/eslint.config.mjs
+++ apps/www/eslint.config.mjs
@@ -1,25 +1,24 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+//? Used for ESM compatibility, since import.meta.dirname is never available
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const eslintConfig = defineConfig([
   {
     languageOptions: {
       parserOptions: {
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: __dirname,
       },
     },
   },
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
